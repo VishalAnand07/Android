36590bdb813076bf1ea76e8ce17542d3f0d01eee
VishalAnand07 <40723537+VishalAnand07@users.noreply.github.com>
2020-07-31 21:25:08 +0530

    hardware sensors

---
+++ app/src/main/java/com/example/hardwaresensors/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.hardwaresensors
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sm = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sensor = sm.getSensorList(Sensor.TYPE_ALL)
+
+        sensor.forEach {
+            Log.i(
+                "SENSORS", """
+                ${it.name},
+                ${it.vendor}
+            """.trimIndent()
+            )
+
+            val acceSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+            val sensorListener = object : SensorEventListener {
+                override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+                }
+
+                override fun onSensorChanged(p0: SensorEvent?) {
+                    Log.i(
+                        "SENSOR VALUES", """
+                        values :
+                        ax : ${p0?.values?.get(0)}
+                        ay : ${p0?.values?.get(1)}
+                        az : ${p0?.values?.get(2)}
+                    """.trimIndent()
+                    )
+
+                    val ax = p0?.values?.get(0) ?: 0f
+                    val ay = p0?.values?.get(1) ?: 0f
+                    val az = p0?.values?.get(2) ?: 0f
+
+                    background.setBackgroundColor(Color.rgb(
+                        (((ax +12 )/ 24) * 255).toInt(),
+                        (((ay +12 )/ 24) * 255).toInt(),
+                        (((az +12 )/ 24) * 255).toInt()
+                    ))
+
+
+                }
+
+            }
+            sm.registerListener(sensorListener, acceSensor, 100000 * 60)
+        }
+    }
+}
